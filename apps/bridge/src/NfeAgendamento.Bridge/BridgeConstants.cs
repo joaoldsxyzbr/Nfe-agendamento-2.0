a59db646044acb92c34f1eafe4efa513c8b45e33
João <contato@joaolds.xyz.br>
2026-09-08 09:26:19 -0300

    feat: fixar endpoint local do bridge

---
+++ apps/bridge/src/NfeAgendamento.Bridge/BridgeConstants.cs
@@ -0,0 +1,7 @@
+namespace NfeAgendamento.Bridge;
+
+public static class BridgeConstants
+{
+    public const string ApiPrefix = "/api/v1";
+    public const string ListenUrl = "http://127.0.0.1:17345";
+}
